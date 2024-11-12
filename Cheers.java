379da2e0921a0b8c6b2db117c67874eac92276cb
Nathan Orlowski <nathan@Someone.local>
2024-11-12 09:39:18 +0200

    first upload

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,25 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+            String input = args[0];
+            int number = Integer.parseInt(args[1]);
+    
+            String anCharacters = "AEFHILMNORSX";
+    
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+    
+                if (anCharacters.indexOf(c) >= 0) {
+                    System.out.println("Give me an " + c +": "+ c + "!");
+                } else {
+                    System.out.println("Give me a "  + c +": "+ c + "!");
+                }
+            }
+    
+            System.out.println("What does that spell?");
+            for (int j = 0; j < number; j++) {
+                System.out.println(input + "!!!");
+            }
         }
-}
+    }
+    
