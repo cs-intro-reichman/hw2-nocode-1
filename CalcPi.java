c1c4ea079e7015b90c4fe896194250ac1db0e874
nocode-1 <nathan.orlowski@post.runi.ac.il>
2024-11-18 15:26:17 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,27 @@
-// Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+    public static void main(String[] args) {
+
+        int input = Integer.parseInt(args[0]);
+        if (input < 0) {
+            System.out.println("Please enter correct number.");
+            return;
+        }
+
+
+        if (args.length != 1) {
+            System.out.println("Usage: java CalcPi <number_of_terms>");
+            return;
+        }
+
+     
+        double piApproximation = 0.0;
+        for (int i = 0; i < input; i++) {
+            piApproximation += Math.pow(-1, i) / (2 * i + 1);
+        }
+        piApproximation *= 4;
+
+        // Print the results
+        System.out.printf("pi according to Java: %.15f%n", Math.PI);
+        System.out.printf("pi, approximated:     %.15f%n", piApproximation);
+    }
 }
