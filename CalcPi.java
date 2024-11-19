1b061b675e4d2c3691889c259114bea5dd9c377a
nocode-1 <nathan.orlowski@post.runi.ac.il>
2024-11-19 11:05:24 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -22,6 +22,6 @@ public class CalcPi {
 
         // Print the results
         System.out.printf("pi according to Java: %.14f%n", Math.PI);
-        System.out.printf("pi, approximated:     %.14f%n", piApproximation);
+        System.out.printf("pi, approximated:     %.15f%n", piApproximation);
     }
 }
